667b57baf0c737e6c022233c1935c740778f5db0
Alex Max <kin0vari@mail.ru>
2024-06-16 16:36:40 +0700

    Comment

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CompareNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            if (number1 == number2)
+            {
+                Console.WriteLine("Числа равны.");
+            }
+            else if (number1 > number2)
+            {
+                Console.WriteLine("Первое число больше второго.");
+            }
+            else
+            {
+                Console.WriteLine("Первое число меньше второго.");
+            }
+        }
+    }
+}
